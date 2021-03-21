59de85e40be82b0d4a34822ab85cf4efc700dd9a
taeinlab <79757866+taeinlab@users.noreply.github.com>
2021-03-21 23:19:10 +0900

    Delete 20210305_2019312512_Taein_IDcard directory

--- 20210305_2019312512_Taein_IDcard/20210305_2019312512_Taein_IDcard(2).R
+++
@@ -1,3 +0,0 @@
-message("name : Taein Lee")
-message("Department : Department of Psychology")
-message("ID Num. : 2019312512")
--- 20210305_2019312512_Taein_IDcard/20210305_2019312512_Taein_IDcard.R
+++
@@ -1,6 +0,0 @@
-# Date : 2021.03.05
-
-
-print("name : Taein Lee")
-print("Department : Department of Psychology")
-print("ID Num. : 2019312512")
