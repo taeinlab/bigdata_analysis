8a50691661b8dc0b634b7ea11829fa05ba2cc8c4
taeinlab <79757866+taeinlab@users.noreply.github.com>
2021-03-21 23:18:54 +0900

    Delete 20210227_2019312512_taein directory

--- 20210227_2019312512_taein/20210227_2019312512_taein_script.R
+++
@@ -1,8 +0,0 @@
-#This example is for printing my information shortly
-# Date : 2021.2.27
-# Author : lee tae in
-
-print("welcome to R studio")
-print("my name is lee tae in")
-
-message("welcome to R studio")
